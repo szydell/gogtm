a14ef402c4ace7538f49e729b6b3f1ea9b8621b6
szydell <marcin@szydelscy.pl>
2016-07-13 16:59:29 -0400

    _gtmaccess.m init

---
+++ _gtmaccess.m
@@ -0,0 +1,43 @@
+%gtmaccess    ; entry points to access GT.M
+    quit
+    ;
+init(error)
+    set $ztrap="new tmp set error=$ecode set tmp=$piece($ecode,"","",2) quit:$quit $extract(tmp,2,$length(tmp)) quit"
+    quit:$quit 0 quit
+    ;
+set(var,value,error)
+    set @var=value
+    quit:$quit 0 quit
+    ;
+get(var,opt,value,error)
+    set value=$GET(@var,opt)
+    quit:$quit 0 quit
+    ;
+kill(var,error)
+    kill @var
+    quit:$quit 0 quit
+    ;
+zkill(var,error)
+    zkill @var
+    quit:$quit 0 quit
+    ;
+xecute(var,error)
+    xecute var
+    quit:$quit 0 quit
+    ;
+order(var,value,error)
+    set value=$order(@var)
+    quit:$quit 0 quit
+    ;
+revorder(var,value,error)
+    set value=$order(@var,-1)
+    quit:$quit 0 quit
+    ;
+query(var,value,error)
+    set value=$query(@var)
+    quit:$quit 0 quit
+    ;
+lock(var,error)
+    lock @var
+    quit:$quit 0 quit
+    ;
