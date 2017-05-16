739607f12acd00870aa7d815fbba84f9e2a3738e
Marcin Szydelski <marcin@szydelscy.pl>
2017-05-16 17:49:24 +0200

    reboot

--- gogtm_test.go
+++ gogtm_test.go
@@ -1,13 +1,1 @@
-package gogtm_test
 
-import (
-  "gogtm"
-  "fmt"
-)
-
-func ExampleSet() {
-  err := gogtm.Set("^test","aaa")
-  if err != nil {
-    fmt.Println("gogtm.Set failed: ", err.Error())
-  }
-}
