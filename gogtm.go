e57788517ee1e9d0949112765e17a4c527509597
Marcin Szydelski <marcin@szydelscy.pl>
2017-05-16 23:07:42 +0200

    first tests

--- gogtm.go
+++ gogtm.go
@@ -1,21 +1,52 @@
 //Package gogtm enables multithreaded access to GT.M database
 package gogtm
 
+import "errors"
+import "github.com/szydell/mstools"
+
 type gtmenv struct {
-	gtmgbldir     string
-	gtmIcuVersion string
-	gtmChset      string
-	gtmLog        string
-	gtmTmp        string
-	gtmDist       string
-	hugeTbl       bool
+	gbldir  string
+	icu     string
+	chset   string
+	log     string
+	tmp     string
+	dist    string
+	hugeTbl bool
 }
 
-type gogtmenv struct {
-	db gtmenv
+func (env *gtmenv) checkParams() error {
+	if env.gbldir == "" {
+		return errors.New("location of database gld file not set")
+	}
+	if tmp, err := mstools.FileOrDir(env.gbldir); tmp != 'f' || err != nil {
+		return errors.New("gbldir does not point to a file")
+	}
+	switch env.chset {
+	case "M":
+	case "UTF-8":
+		if env.icu == "" {
+			return errors.New("if using UTF-8 libicu must be set")
+		}
+	default:
+		return errors.New("set proper chset (M or UTF-8)")
+	}
+	if env.chset == "UTF-8" && env.icu == "" {
+
+	}
+	if tmp, err := mstools.FileOrDir(env.log); tmp != 'd' || err != nil {
+		return errors.New("location for db logs not set or is not a directory")
+	}
+	if tmp, err := mstools.FileOrDir(env.tmp); tmp != 'd' || err != nil {
+		return errors.New("location for db temporary files not set or is not a directory")
+	}
+	if tmp, err := mstools.FileOrDir(env.dist); tmp != 'd' || err != nil {
+		return errors.New("location of gt.m engine not set or is not a directory")
+	}
+
+	return nil
 }
 
-//Start initiates connection to GT.M database
-func Start() {
+//Start do all needed checks and initiates connection to GT.M database
+func Start(db gtmenv) {
 
 }
--- gogtm_test.go
+++ gogtm_test.go
@@ -1,1 +1,87 @@
-package gogtm_test
+package gogtm
+
+import "testing"
+
+func TestCheckParams(t *testing.T) {
+
+	env := &gtmenv{gbldir: ""}
+
+	if err := env.checkParams(); err == nil {
+		t.Error("empty glbdir should be returned as an error")
+	} else {
+		t.Logf("returned: %s", err)
+	}
+
+	env = &gtmenv{
+		gbldir: "gogtm_test.go",
+		chset:  ""}
+	if err := env.checkParams(); err == nil {
+		t.Error("empty chset should be returned as an error")
+	} else {
+		t.Logf("returned: %s", err)
+	}
+	env = &gtmenv{
+		gbldir: "gogtm_test.go",
+		chset:  "UTF-8"}
+	if err := env.checkParams(); err == nil {
+		t.Error("utf with no icu set shoud return an error")
+	} else {
+		t.Logf("returned: %s", err)
+	}
+
+	env = &gtmenv{
+		gbldir: "gogtm_test.go",
+		chset:  "M",
+		log:    "gogtm_test.go"}
+	if err := env.checkParams(); err == nil {
+		t.Error("file set as a log destination should return an error")
+	} else {
+		t.Logf("returned: %s", err)
+	}
+
+	env = &gtmenv{
+		gbldir: "gogtm_test.go",
+		chset:  "M",
+		log:    "/tmp",
+		tmp:    ""}
+	if err := env.checkParams(); err == nil {
+		t.Error("unknown dist destination should return an error")
+	} else {
+		t.Logf("returned: %s", err)
+	}
+
+	env = &gtmenv{
+		gbldir: "gogtm_test.go",
+		chset:  "M",
+		log:    "/tmp",
+		tmp:    "/tmp",
+		dist:   ""}
+	if err := env.checkParams(); err == nil {
+		t.Error("unknown dist destination should return an error")
+	} else {
+		t.Logf("returned: %s", err)
+	}
+	env = &gtmenv{
+		gbldir: "gogtm_test.go",
+		chset:  "M",
+		log:    "/tmp",
+		tmp:    "/tmp",
+		dist:   "/tmp"}
+	if err := env.checkParams(); err != nil {
+		t.Error("this one should be ok")
+	} else {
+		t.Logf("returned: %s", env)
+	}
+	env = &gtmenv{
+		gbldir: "gogtm_test.go",
+		chset:  "UTF-8",
+		icu:    "5.0",
+		log:    "/tmp",
+		tmp:    "/tmp",
+		dist:   "/tmp"}
+	if err := env.checkParams(); err != nil {
+		t.Error("this one should be ok")
+	} else {
+		t.Logf("returned: %s", env)
+	}
+}
