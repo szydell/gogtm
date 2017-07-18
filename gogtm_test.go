2ca9e82b8a1763978a4ead759d95b01f485c0f0f
Marcin Szydelski <marcin@szydelscy.pl>
2017-07-18 15:07:19 +0200

    ExampleGet

--- gogtm_test.go
+++ gogtm_test.go
@@ -12,3 +12,15 @@ func ExampleSet() {
 		fmt.Println("gogtm.Set failed: ", err.Error())
 	}
 }
+
+func ExampleGet() {
+	data, err := gogtm.Get("^test", "ababababababababa")
+	if err != nil {
+		fmt.Println("gogtm.Get failed: ", err.Error())
+	}
+	if data == "ababababababababa" {
+		fmt.Println("Global ^test empty, default value returned")
+	} else {
+		fmt.Println("Value of global ^test is: " + data)
+	}
+}
