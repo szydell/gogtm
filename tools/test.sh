3db2674d2cbc5a23da5bdb1411ade7b26bd00f95
Marcin Szydelski <marcin@szydelscy.pl>
2018-02-15 00:59:06 +0100

    travis +1

--- tools/test.sh
+++ tools/test.sh
@@ -8,7 +8,7 @@ mkdir /tmp/gtm_install
 cd /tmp/gtm_install
 wget https://downloads.sourceforge.net/project/fis-gtm/GT.M-amd64-Linux/V6.3-001A/gtm_V63001A_linux_x8664_pro.tar.gz
 tar -xzf gtm_V63001A_linux_x8664_pro.tar.gz
-sudo apt-get install -y libicu-dev icu-devtools
+#sudo apt-get install -y libicu-dev icu-devtools
 sudo rm -rf /opt/fis-gtm/6.3-001A
 sudo ./configure << EOF
 bin
@@ -18,7 +18,7 @@ n
 y
 y
 y
-5.2
+5.0
 y
 y
 y
@@ -27,13 +27,13 @@ rm -rf /tmp/gtm_install
 # Create symbolic link used for gogtm compilation
 export gtm_dist=/opt/fis-gtm/engine
 sudo ln -s /opt/fis-gtm/6.3-001A $gtm_dist
-mkdir /tmp/gtm
+mkdir -p /tmp/gtm
 export PATH=$gtm_dist:$PATH
 export gtm_chset=UTF-8
 export gtmgbldir=/tmp/gtm.gld
 export gtm_tmp=/tmp/gtm
-export gtm_icu_version=5.2
+export gtm_icu_version=5.0
 alias gde="$gtm_dist/mumps -run GDE"
-echo "exit" | $gtm_dist/mumps -run GDE; stty sane
-
+export gtmroutines=$gtm_dist/libgtmutil.so
+#$gtm_dist/mumps -run GDE
 #source /opt/fis-gtm/6.3-001A/gtmprofile
