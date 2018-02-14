c14a354de37df36a364f9a69d5f583fdadc6078a
Marcin Szydelski <marcin@szydelscy.pl>
2018-02-14 23:39:56 +0100

    travis +1

--- tools/install_engine_travis.sh
+++ tools/install_engine_travis.sh
@@ -8,7 +8,7 @@ mkdir /tmp/gtm_install
 cd /tmp/gtm_install
 wget https://downloads.sourceforge.net/project/fis-gtm/GT.M-amd64-Linux/V6.3-001A/gtm_V63001A_linux_x8664_pro.tar.gz
 tar -xzf gtm_V63001A_linux_x8664_pro.tar.gz
-sudo apt-get install -y libicu-dev libicu
+sudo apt-get install -y libicu-dev icu
 sudo rm -rf /opt/fis-gtm/6.3-001A
 sudo ./configure << EOF
 bin
